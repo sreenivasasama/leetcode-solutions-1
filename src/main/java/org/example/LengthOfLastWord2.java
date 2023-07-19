83421eb55bcf8f46591bc43f59fc0bfaf7ec16b6
sreenivasasama <sreenivasa.sama@gmail.com>
2023-07-19 06:44:21 -0400

    Len of last word sol2 added

--- src/main/java/org/example/LengthOfLastWord2.java
+++ src/main/java/org/example/LengthOfLastWord2.java
@@ -2,29 +2,30 @@ package org.example;
 
 import java.util.Scanner;
 
-public class LengthOfLastWord {
+public class LengthOfLastWord2 {
 
     public static void main(String[] args) {
-        //solution to find the length of last string by reversing the string
+        //solution to find the length of last string without reversing the string
         System.out.println("Hello there! enter a String: ");
         //take input from console
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         System.out.println("the entered string is " + s);
         int len = s.length();
-        StringBuilder sb = new StringBuilder(s);
-        // reverse the string, so that first word can be now the last word of original string
-        sb.reverse();
         int i = 0;
         int count = 0;
-        //remove initial spaces
-        while(i<len && sb.charAt(i) == ' ') {
-            i++;
-        }
-        //count the characters of first string if found
-        while(i<len && sb.charAt(i) != ' ') {
-            count++;
-            i++;
+        while(i<len) {
+            //initialize the count to 0
+            count = 0;
+            //remove initial spaces
+            while (i < len && s.charAt(i) == ' ') {
+                i++;
+            }
+            //count the characters of each string if found
+            while (i < len && s.charAt(i) != ' ') {
+                count++;
+                i++;
+            }
         }
         //print the last string length and exit
         System.out.println("Length of last string is " + count);
